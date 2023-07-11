f62c20b66cc0301493e7732edf2ca31716df34d5
Julia Benito <julia.benito@babelgroup.com>
2023-07-11 09:00:46 +0200

    simple widget

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Center(child: Text('Hola mundos')),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(child: Text('Hola mundos')),
+      )
     );
   }
 
